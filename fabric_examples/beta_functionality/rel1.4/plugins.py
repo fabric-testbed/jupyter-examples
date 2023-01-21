a2e474b5f7166a27e9f2afe55f7e5c3bd3b48583
Komal Thareja <kthare10@renci.org>
2023-01-20 19:20:53 -0500

    Release 1.4 notebooks

--- fabric_examples/beta_functionality/rel1.4/plugins.py
+++ fabric_examples/beta_functionality/rel1.4/plugins.py
@@ -11,16 +11,16 @@ class Plugins:
     @staticmethod
     def load():
         n = NetworkService
+        n.change_public_ip = Plugins.change_public_ip
     
-    def update(self, ip_list: list):   
+    def change_public_ip(self, ipv6: list[str] = None, ipv4: list[str] = None):
         labels = self.fim_network_service.labels
         if labels is None:
             labels = Labels()
         if self.fim_network_service.type == ServiceType.FABNetv4Ext:
-            labels = Labels.update(labels, ipv4=ip_list)
+            labels = Labels.update(labels, ipv4=ipv4)
             
         elif self.fim_network_service.type == ServiceType.FABNetv6Ext:
-            labels = Labels.update(labels, ipv6=ip_list)
+            labels = Labels.update(labels, ipv6=ipv6)
         
         self.fim_network_service.set_properties(labels=labels)
-        
