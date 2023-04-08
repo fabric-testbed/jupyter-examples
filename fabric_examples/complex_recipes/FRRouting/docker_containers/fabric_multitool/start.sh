8e0ae0d582852f11c5ccca13c2e84bfd53f0dd92
Paul Ruth <pruth@renci.org>
2023-04-08 09:23:42 -0400

    updated notebooks

--- fabric_examples/complex_recipes/FRRouting/docker_containers/fabric_multitool/start.sh
+++ fabric_examples/complex_recipes/FRRouting/docker_containers/fabric_multitool/start.sh
@@ -18,6 +18,6 @@ sudo ${path}/node_tools/set_netns_ifaces.py -n ${netns}  $devs
 #sudo ip netns exec fabric  ${path}/node_tools/config_routes.py ip_config/routes/routes.json 
 docker exec frrouting sh -c './config/node_tools/config_ifaces.py ./config/ip_config/ifaces/eth*.json' 
 docker exec frrouting sh -c './config/node_tools/config_routes.py ip_config/routes/routes.json'
-docker exec ${container} sudo ./tools/host_tune_redhat.sh 
+#docker exec ${container} sudo ./tools/host_tune_redhat.sh 
 
 cd $cwd
