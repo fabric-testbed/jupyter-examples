22dfae08f0fc3dcf354f85d6d131297764936d62
Hussamuddin Nasir <nasir@netlab.uky.edu>
2023-09-18 11:20:58 -0400

    Modify start tcpdump script

--- fabric_examples/public_demos/KNIT7/Tutorial1_Precision_Time_Measurements_in_FABRIC/tools/start_dump.sh
+++ fabric_examples/public_demos/KNIT7/Tutorial1_Precision_Time_Measurements_in_FABRIC/tools/start_dump.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 node=$1
 interface=$2
+mode=$3
 rm -f nohup.out
-nohup tcpdump -j adapter_unsynced -U -n --time-stamp-precision nano -i ${interface} "icmp[0] == 8" -w ${node}.pcap &
+nohup tcpdump -j adapter_unsynced -U -n --time-stamp-precision nano -i ${interface} "icmp[0] == 8" -w ${node}_${mode}.pcap &
 #nohup tcpdump -j adapter_unsynced -U -n --time-stamp-precision nano -i ${interface} -w ${node}.pcap &
 
 # Write tcpdump's PID to a file
