1439f431322d584c62063cfa1426332e4914ca18
Ilya Baldin <ibaldin@renci.org>
2023-12-06 17:58:08 -0500

    Removing NAT64 configuration from FPGA flashing

--- fabric_examples/acceptance_testing/Acceptance Tests 6.1.2/node_config/ipv6-and-docker-plugins.sh
+++ fabric_examples/acceptance_testing/Acceptance Tests 6.1.2/node_config/ipv6-and-docker-plugins.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# NOTE: obsoleted
 # Setup NAT64 based on https://nat64.net/
-sudo sed -i '/nameserver/d' /etc/resolv.conf
-sudo sh -c 'echo nameserver 2a00:1098:2c::1 >> /etc/resolv.conf' 
-sudo sh -c 'echo nameserver 2a01:4f8:c2c:123f::1 >> /etc/resolv.conf' 
-sudo sh -c 'echo nameserver 2a00:1098:2b::1 >> /etc/resolv.conf'
+#sudo sed -i '/nameserver/d' /etc/resolv.conf
+#sudo sh -c 'echo nameserver 2a00:1098:2c::1 >> /etc/resolv.conf' 
+#sudo sh -c 'echo nameserver 2a01:4f8:c2c:123f::1 >> /etc/resolv.conf' 
+#sudo sh -c 'echo nameserver 2a00:1098:2b::1 >> /etc/resolv.conf'
 
 # install Docker compose and buildx plugins (does not require sudo - installs for this user only)
 
